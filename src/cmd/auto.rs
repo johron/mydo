e3bab3cd3277d4dcaa2cbb2ba00128054652fbec
johron <johron@windows>
2024-03-19 20:25:06 +0100

    fixed bad config system

--- src/cmd/auto.rs
+++ src/cmd/auto.rs
@@ -3,8 +3,16 @@ use execute::Execute;
 
 use crate::util;
 
+fn get_last(input: &str) -> Option<&str> {
+    if let Some(index) = input.rfind('.') {
+        // Add 1 to exclude the dot itself
+        Some(&input[index + 1..])
+    } else {
+        None
+    }
+}
+
 pub fn auto(parameters: &mut Vec<String>) {
-    let presets = util::config::get_presets();
     let file = &parameters[0];
     
     if !path::Path::new(file).exists() {
@@ -12,46 +20,35 @@ pub fn auto(parameters: &mut Vec<String>) {
         process::exit(1);
     }
     
-    let mut found = false;
-
     let shebang = util::shebang::get_shebang(&file).unwrap_or("None".parse().unwrap());
     let shebang_owned = shebang.to_owned();
     let runner = &shebang_owned;
     
     if shebang == "None" {
-        for (key, preset) in presets.unwrap() {
-            if key == "run" || key == "build" {}
             
-            if file.ends_with(key.as_str()) {
-                let runner = preset.as_str()
-                    .expect("Expected a string")
-                    .replace("{file}", file)
-                    .replace("{home}", &util::home::get_home())
-                    .replace("{output}", file.split(".")
-                        .collect::<Vec<&str>>()[0]);
-                
-                let mut to_pass: Vec<&str> = runner.split(" ").collect();
-                let binding = process::Command::new(to_pass[0]);
-                let mut cmd = binding;
-                
-                to_pass.remove(0);
-                cmd.args(to_pass);
+        if file == "run" || file == "build" {
+        } else {
+            let runner = util::config::get_preset(get_last(file).unwrap()).as_str()
+                .expect("Expected a string")
+                .replace("{file}", file)
+                .replace("{home}", &util::home::get_home())
+                .replace("{output}", file.split(".")
+                    .collect::<Vec<&str>>()[0]);
 
-                let now = time::SystemTime::now();
-                cmd.execute_output().unwrap().stdout;
+            let mut to_pass: Vec<&str> = runner.split(" ").collect();
+            let binding = process::Command::new(to_pass[0]);
+            let mut cmd = binding;
 
-                let show_compilation_time = util::config::get_setting("show_time");
-                if show_compilation_time.unwrap() == true {
-                    println!("Time taken: {:.2}s", now.elapsed().unwrap().as_secs_f32());
-                }
+            to_pass.remove(0);
+            cmd.args(to_pass);
 
-                found = true;
+            let now = time::SystemTime::now();
+            cmd.execute_output().unwrap().stdout;
+
+            let show_compilation_time = util::config::get_setting("show_time");
+            if show_compilation_time.unwrap() == true {
+                println!("Time taken: {:.2}s", now.elapsed().unwrap().as_secs_f32());
             }
-        }
-
-        if !found {
-            eprintln!("Error: No preset found for '{}'", file);
-            process::exit(1)
         }
     } else {
         let binding = process::Command::new(runner);
--- src/util/config.rs
+++ src/util/config.rs
@@ -1,96 +1,75 @@
-use std::{fs, process, path};
-use serde_json::{Map, Value};
+use std::{fs, process};
+use serde_json::{Value};
 use crate::util;
 
-fn get_path(project_root: bool) -> String {
-    let home = util::home::get_home().to_string();
+fn read_main_config() -> Value {
+    let path = util::home::get_home().to_string() + "/.mydo/mydo.json";
+    
+    let binding = fs::read_to_string(path)
+        .unwrap_or_else(|err| panic!("Failed to read config file: {}", err));
 
-    let from_root = "mydo.json";
-    let binding = home + "/.mydo/mydo.json";
-    let from_home = binding.as_str();
-
-    if project_root {
-        if path::Path::new(from_root).exists() {
-            return from_root.to_string();
-        } else if path::Path::new(from_home).exists() {
-            return from_home.to_string();
-        } else {
-            eprintln!("Error: Project config and home config file does not exist.");
-            process::exit(1);
-        }
-    } else {
-        if path::Path::new(from_home).exists() {
-            return from_home.to_string();
-        } else {
-            eprintln!("Error: Home config file does not exist.");
-            process::exit(1);
-        }
-    }
+    let config: Value = serde_json::from_str(&binding)
+        .unwrap_or_else(|err| panic!("Failed to parse config JSON: {}", err));
+    
+    config
 }
 
-pub fn get_presets() -> Option<Map<String, Value>> {
-    let path = get_path(true);
+fn read_project_config() -> Value {
+    let path = "mydo.json";
+
     let binding = fs::read_to_string(path)
         .unwrap_or_else(|err| panic!("Failed to read config file: {}", err));
 
     let config: Value = serde_json::from_str(&binding)
         .unwrap_or_else(|err| panic!("Failed to parse config JSON: {}", err));
 
-    config["presets"].as_object().map(|obj| obj.clone())
-}
-
-pub fn get_init(init: &str) -> Option<Value> {
-    let path = get_path(false);
-    let binding = fs::read_to_string(path)
-        .unwrap_or_else(|err| panic!("Failed to read config file: {}", err));
-
-    let config: Value = serde_json::from_str(&binding)
-        .unwrap_or_else(|err| panic!("Failed to parse config JSON: {}", err));
-
-    if let Some(inits) = config["inits"].as_object() {
-        if let Some(init_value) = inits.get(init) {
-            return Some(init_value.clone());
-        } else {
-            eprintln!("Error: Init '{}' not found in mydo.json", init);
-        }
-    } else {
-        eprintln!("Error: 'inits' object not found in mydo.json");
-    }
-
-    eprintln!("Error: No data in '{}' init", init);
-    process::exit(1);
+    config
 }
 
 pub fn get_setting(setting: &str) -> Option<Value> {
-    let path = get_path(false);
-    let binding = fs::read_to_string(path)
-        .unwrap_or_else(|err| panic!("Failed to read config file: {}", err));
-
-    let config: Value = serde_json::from_str(&binding)
-        .unwrap_or_else(|err| panic!("Failed to parse config JSON: {}", err));
+    let config = read_main_config();
 
     if let Some(settings) = config["settings"].as_object() {
         if let Some(setting_value) = settings.get(setting) {
             return Some(setting_value.clone());
         } else {
-            eprintln!("Error: Setting '{}' not found in mydo.json", setting);
+            eprintln!("Error: Setting '{}' not found in ~/.mydo/mydo.json", setting);
         }
     } else {
-        eprintln!("Error: 'settings' object not found in mydo.json");
+        eprintln!("Error: 'settings' object not found in ~/.mydo/mydo.json");
     }
 
     eprintln!("Error: No data in '{}' setting", setting);
     process::exit(1);
 }
 
-pub fn get_preset(preset: &str) -> Value {
-    let presets = get_presets().unwrap();
+pub fn get_init(init: &str) -> Option<Value> {
+    let config = read_main_config();
 
-    for (p, args) in presets {
-        if p == preset {
-            return args;
+    if let Some(settings) = config["inits"].as_object() {
+        if let Some(setting_value) = settings.get(init) {
+            return Some(setting_value.clone());
+        } else {
+            eprintln!("Error: Init '{}' not found in ~/.mydo/mydo.json", init);
         }
+    } else {
+        eprintln!("Error: 'inits' object not found in ~/.mydo/mydo.json");
     }
+
+    eprintln!("Error: No data in '{}' init", init);
+    process::exit(1);
+}
+
+pub fn get_preset(preset: &str) -> Value {
+    let config = read_project_config();
+
+    let presets = config["presets"].as_object().map(|obj| obj.clone()).unwrap();
+    
+   for (p, args) in presets {
+       if p == preset {
+           return args;
+       }
+   }
 
     eprintln!("Error: Requested preset '{preset}' does not exist");
     process::exit(1);
